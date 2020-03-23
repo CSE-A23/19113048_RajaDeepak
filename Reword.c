d8424f210be0b12771ba24dd90130e0cbb503804
CSE-A23 <csea23@outlook.in>
2020-03-23 19:48:43 +0530

    Create Reword.c

---
+++ Reword.c
@@ -0,0 +1,37 @@
+#include<iostream.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+int n,i;
+char arr[100];
+clrscr();
+cout<<"\nenter a word to be checked";
+cin>>"s">>arr;
+for(i=0;i<n;i++)
+{
+if(n>=6&&n<=20)
+{
+if(arr[i]=='r')
+{
+if(arr[i]!=' ');
+cout<<"\nreeword";
+}
+}
+for(i= ;i<n;i++)
+{
+if(n>= && n<= ) 
+{
+if(arr[i]=='r') 
+{
+if(arr[i]!=' ');
+cout<<"\nreeword";
+}
+}
+else
+{
+cout<<"\nnay";
+}
+}
+getch();
+}
